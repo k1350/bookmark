2f17e0d9fee1616c57214269f1f403e4acb37a12
Kimiko Mitamura <pom96egran46ate@gmail.com>
2024-11-04 15:46:48 +0900

    chore: 不要な記述削除

--- packages/novel-bookmark/src/constants.ts
+++ packages/novel-bookmark/src/constants.ts
@@ -3,4 +3,3 @@ export const NOVEL_BOOKMARK_P_ID_PREFIX = "element-p-";
 export const NOVEL_BOOKMARK_P_DATA_NAME = "data-novel-bookmark-id";
 export const NOVEL_BOOKMARK_INTERSECTING_DATA_NAME =
   "data-novel-bookmark-intersecting";
-const a = 1;
